efd281b186eceec117dc1411f11e788a4508bc77
hassan mirza <hassan0342@users.noreply.github.com>
2020-12-23 20:34:09 +0500

    working on rota request

--- resources/views/admin/rota/request/partial/form.blade.php
+++ resources/views/admin/rota/request/partial/form.blade.php
@@ -1,42 +1,50 @@
-
-
-
-       <div class="form-group">
-        {{-- <div> --}}
-            <input type="checkbox" name="is_general" value="general" onclick="toggle_duty_date(this);">
-            {!! Form::label('general','Is General') !!}
-        {{-- </div> --}}
-    </div>
-    <div class="form-group" id="duty_date">
-        {!! Form::label('dutydate','Duty Date') !!}
-        <div>
-            {!! Form::date('dutydate',null, ['class' => 'form-control',
-            'data-parsley-required'=>'true','id'=>'duty_date',
-            'data-parsley-trigger'=>'change',
-            'placeholder'=>'dutydate','required']) !!}
-        </div>
-    </div>
-
-
-
-
+<div class="form-group">
+    {{-- <div> --}}
+    <input type="checkbox" name="is_general" value="general" onclick="toggle_form(this,'duty_date');">
+    {!! Form::label('general','Is General') !!}
+    {{-- </div> --}}
+</div>
 <div class="form-group" id="duty_date">
+    {!! Form::label('dutydate','Duty Date') !!}
+    <div>
+        {!! Form::date('dutydate',null, ['class' => 'form-control',
+        'data-parsley-required'=>'true','id'=>'duty_date',
+        'data-parsley-trigger'=>'change',
+        'placeholder'=>'dutydate','required']) !!}
+    </div>
+</div>
+
+{{-- <div class="form-group">
+    <input type="checkbox" name="specific_week" value="specific_week" checked
+        onclick="toggle_form(this,'specific_week_day');">
+    {!! Form::label('specific_week','Select Week Day') !!}
+</div>
+
+
+<div class="form-group" id="specific_week_day">
     {!! Form::label('weekday_id','Week Day') !!}
     <div>
         {!! Form::select('weekday_id',$weekdays,null, ['class' => 'form-control',
         'data-parsley-required'=>'true',
-        'data-parsley-trigger'=>'change',
-        'required']) !!}
+        'data-parsley-trigger'=>'change'
+        ]) !!}
     </div>
-</div>
+</div> --}}
 
 
 {{-- <input name="doctor_id" id="doctor_id" value="{{$doctor->id}}" hidden> --}}
 
-<div class="form-group" id="duty_date">
-    {!! Form::label('shiftday','Shift Day') !!}
+<div class="form-group">
+    {{-- <div> --}}
+    <input type="checkbox" name="on_shift" value="1" checked onclick="toggle_form(this,'shift');">
+    {!! Form::label('shift','Select Shift') !!}
+    {{-- </div> --}}
+</div>
+
+<div class="form-group" id="shift">
+    {!! Form::label('shiftday','Shift') !!}
     <div>
-        {!! Form::select('shiftday',['morning','evenging','night'],null, ['class' => 'form-control',
+        {!! Form::select('shiftday',$shifts,null, ['class' => 'form-control',
         'data-parsley-required'=>'true',
         'data-parsley-trigger'=>'change',
         'required']) !!}
@@ -45,16 +53,13 @@
 
 
 
-   {{-- <input type="radio" name="duty" value="wantduty">Want duty<br> --}}
-  {{-- <input type="radio"  name="duty" value="wantoff">Want Off<br> --}}
+{{-- <input type="radio" name="duty" value="wantduty">Want duty<br> --}}
+{{-- <input type="radio"  name="duty" value="wantoff">Want Off<br> --}}
 
 
 
 <div class="form-group col-md-12">
 </div>
-
-
-
 
 
 <div class="col-md-5 pull-left">
@@ -70,15 +75,9 @@
 @section('app_jquery')
 
 <script>
+    function toggle_form(e,div) {
 
-function toggle_duty_date(e) {
-    if(e.checked){
-        $('#duty_date').css('display','none');
-    }
-    else{
-        $('#duty_date').css('display','block');
-    }
-
+    $('#'+div).toggle('show');
 }
 
 
