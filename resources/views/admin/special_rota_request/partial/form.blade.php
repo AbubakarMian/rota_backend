a44041ffff9085127dbfcdb3433d4efcb2bf5031
hassan mirza <hassan0342@users.noreply.github.com>
2021-01-20 13:17:51 +0500

    rota generate pattern and special rota request fixed

--- resources/views/admin/special_rota_request/partial/form.blade.php
+++ resources/views/admin/special_rota_request/partial/form.blade.php
@@ -1,5 +1,15 @@
+<div class="form-group">
+    {!! Form::label('doctor') !!}
 
-  <div class="form-group">
+    <select id="select-example" class="form-control" name="doctor_id" placeholder="Select doctor...">
+        @foreach ($doctor as $d)
+        <option value="{{$d->id}}">{{$d->user->name}}</option>
+
+        @endforeach
+    </select>
+</div>
+
+<div class="form-group">
     {!! Form::label('dutydate','Duty
     Date') !!}
     <div>
@@ -12,55 +22,32 @@
 </div>
 
 
- <input type="radio" name="duty" value="wantduty"  id="myRadio" checked><span class="myduty">Want duty</span>
-<input type="radio"  name="duty" value="wantoff"><span class="myoff">Want off</span>
 
-
-
-
-<div class="form-group" id="shift">
-    {!! Form::label('shiftday','Shift') !!}
+<div class="form-group">
     <div>
-        {!! Form::select('shiftday',$shifts,null, ['class' => 'form-control',
-        'data-parsley-required'=>'true',
-        'data-parsley-trigger'=>'change',
-        'required']) !!}
+        <input type="radio" name="duty" value="wantduty" id="myRadio" onchange="toggle_element('shift');" checked>
+        <span class="myduty" onclick="">Want duty</span>
+        <input type="radio" name="duty" value="wantoff" onchange="toggle_element('shift');">
+        <span class="myoff">Want off</span>
     </div>
 </div>
 
+<div>
+    <div class="form-group" id="shift">
+        {!! Form::label('shiftday','Shift') !!}
+        <div>
+            {!! Form::select('shiftday',$shifts,null, ['class' => 'form-control',
+            'data-parsley-required'=>'true',
+            'data-parsley-trigger'=>'change',
+            'required']) !!}
+        </div>
+    </div>
 
-<div class="form-group">
-    {!! Form::label('doctor') !!}
 
-
-
-
-  <select id="select-example" class="form-control"  name="doctor_id"  placeholder="Select doctor...">
-    @foreach ($doctor as $d)
-    <option value="{{$d->id}}">{{$d->user->name}}</option>
-
-    @endforeach
-</select>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <div class="form-group col-md-12">
 </div>
-
-
-
-
 
 <div class="col-md-5 pull-left">
     <div class="form-group text-center">
@@ -73,12 +60,13 @@
 
 @section('app_jquery')
 <script>
-
-
-
-
     function validateForm(){
         return true;
+    }
+
+    function toggle_element(element){
+        console.log('toggle_element',element);
+        $("#"+element).toggle();
     }
 
 </script>
